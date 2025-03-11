3cbe4d40758664f882aca6e2b98b257d6ef9f9ec
Pragyan Thapa <pragyanthapa44@gmail.com>
2025-03-11 16:31:51 +0530

    changes made

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,6 +1,12 @@
 import app from "./src/app.js";
 
 
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
